a75daa42d11af2cec0693fc2aaedc3841dee6d9c
Vladimir Popov <popovdimir@gmail.com>
2016-02-01 10:55:06 +0100

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.shape.Circle;
+
 public class Controller {
+
+    public Circle kreis;
+
+    public void moving(Event event) {
+        double x = kreis.getCenterX();
+        double y = kreis.getCenterY();
+        kreis.setTranslateX(x+2);
+        kreis.setTranslateY(y+2);
+    }
 }
